27dea43a40b6cda9b6a66d43f4a0d5cd7fa5add1
mohammad ghorbani <mohammad ghorbani>
2017-05-09 22:15:45 +0430

    Sending & Receiving Data with Server Is Done

--- Chat.py
+++ Chat.py
@@ -4,9 +4,16 @@ from PyQt4 import QtGui
 from PyQt4 import QtCore
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+from socket import *
 
 class Window(QtGui.QMainWindow):
     def __init__(self):
+        
+        self.serverName='m-gh.info'
+        self.serverPort=12000
+        self.clientSocket=socket(AF_INET,SOCK_STREAM)
+        self.clientSocket.connect((self.serverName,self.serverPort))
+
         super(Window,self).__init__()
         self.setGeometry(50,50,500,300)
         self.setFixedSize(500,300)
@@ -16,11 +23,15 @@ class Window(QtGui.QMainWindow):
         myappid = 'mycompany.myproduct.subproduct.version'  # arbitrary string
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
-        textbox_Message = QLineEdit(self)
-        textbox_Message.setAlignment(Qt.AlignTop)
-        textbox_Message.move(5, 230)
-        textbox_Message.resize(380,65)
-        textbox_Message.setPlaceholderText("Type Your Message Here....")
+        self.textbox_Message = QLineEdit(self)
+        self.textbox_Message.setAlignment(Qt.AlignTop)
+        self.textbox_Message.move(5, 230)
+        self.textbox_Message.resize(380,65)
+        self.textbox_Message.returnPressed.connect(self.Send_Message)
+        self.textbox_Message.setPlaceholderText("Type Your Message Here....")
+        f = self.textbox_Message.font()
+        f.setPointSize(18) # sets the size to 27
+        self.textbox_Message.setFont(f)
 
         textbox_Messages_box=QLineEdit(self)
         textbox_Messages_box.setAlignment(Qt.AlignTop)
@@ -33,14 +44,24 @@ class Window(QtGui.QMainWindow):
 
     def home(self):
         btn_Send_Text=QtGui.QPushButton("Send",self)
-        btn_Send_Text.clicked.connect(self.close_application)
+        btn_Send_Text.clicked.connect(self.Send_Message)
         btn_Send_Text.move(390,233)
 
         btn_Send_File=QtGui.QPushButton("File",self)
-        btn_Send_File.clicked.connect(self.close_application)
+        btn_Send_File.clicked.connect(self.Send_File)
         btn_Send_File.move(390,265)
         self.show()
     
+    def Send_Message(self):
+        sentence=self.textbox_Message.text()
+        print(sentence)
+        self.clientSocket.sendto(sentence.encode('utf-8'),(self.serverName, self.serverPort))
+        modifiedSentence=self.clientSocket.recv(1024)
+        print ('From Server:',modifiedSentence.decode())
+
+    def Send_File(self):
+        print("Sending File....")
+
     def close_application(self):
         print("\nClosing...\n")
         sys.exit()
