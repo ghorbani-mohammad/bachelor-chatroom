a373dc2da33bf60ce457ee7137e52cc58c3004c2
mohammad ghorbani <mohammad ghorbani>
2017-05-09 16:11:38 +0430

    QT 1

---
+++ Chat.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt4 import QtGui
+
+app=QtGui.QApplication(sys.argv)
+
+window=QtGui.QWidget()
+window.setGeometry(50,50,500,300)
+window.setWindowTitle("Chater")
+
+window.show()
+
+
+sys.exit(app.exec_())
